ef0e16ceb561efc1e85cc6dc9dcd4d35227be2c4
Arvid Lundin <arvid.lundin@student.forsbergsskola.se>
2022-04-05 12:29:27 +0200

    added some stuff to sound scene

---
+++ Assets/Test_Folders/SOUND/Scripts/MusicController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public FMODUnity.EventReference musicEvRef;
+    private FMOD.Studio.EventInstance musicEvInst;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeMusicHealthParam(int _val)
+    {
+        musicEvInst.setParameterByName
+    }
+}
