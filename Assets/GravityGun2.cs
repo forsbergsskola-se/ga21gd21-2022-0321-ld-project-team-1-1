2e3d5f1345110e23dd50f0336b575f29cc8c0665
Erick Vaghi <91052540+ErickVaghi@users.noreply.github.com>
2022-04-12 16:50:40 +0200

    materials now changes when you look and grab objects with tags

--- Assets/GravityGun2.cs
+++ Assets/GravityGun2.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GravityGun2 : MonoBehaviour
 {
    [SerializeField] private string interactiveTag = "Interactive";
+   
+   [SerializeField] private Material grabbedMaterial;
+   [SerializeField] private Material defaultMaterial;
+
+   [SerializeField] private GameObject SelectionManager;
    
    private float Yrot;
    private RaycastHit hit;
@@ -22,15 +28,21 @@ public class GravityGun2 : MonoBehaviour
       if (Input.GetMouseButtonDown(1) && Physics.Raycast(transform.position, transform.forward, out hit, 5) &&
           hit.transform.GetComponent<Rigidbody>() && hit.transform.CompareTag(interactiveTag))
       {
+         SelectionManager.SetActive(false);
+         //defaultMaterial = selectionRenderer.material; //save object original material
          grabbedOBJ = hit.transform.gameObject;
+         var selectionRenderer = grabbedOBJ.GetComponent<Renderer>();
+         selectionRenderer.material = grabbedMaterial; //change material when hold
             //Audio
             //Debug.Log("make noise");
             Gravitygun2.Gravitygun2Audio();
       }
       else if (Input.GetMouseButtonUp(1))
       {
+         SelectionManager.SetActive(true);
          grabbedOBJ = null;
-            Gravitygun2.Gravitygun2AudioStop();
+         Gravitygun2.Gravitygun2AudioStop();
+         
       }
       if (grabbedOBJ)
       {
