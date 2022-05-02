1bde344fd0bdcf5b3e8fbca4d58a7a48cfefee3d
Erick Vaghi <vaghi.erick@gmail.com>
2022-05-02 15:22:17 +0200

    pause menu added

--- Assets/SampleScenes/Menu/Scripts/PauseMenu.cs
+++ Assets/SampleScenes/Menu/Scripts/PauseMenu.cs
@@ -1,19 +1,22 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    private Toggle m_MenuToggle;
+    //private Toggle m_MenuToggle;
 	private float m_TimeScaleRef = 1f;
     private float m_VolumeRef = 1f;
     private bool m_Paused;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject gameUI;
 
 
-    void Awake()
+    /*void Awake()
     {
         m_MenuToggle = GetComponent <Toggle> ();
-	}
+	}*/
 
 
     private void MenuOn ()
@@ -25,6 +28,11 @@ public class PauseMenu : MonoBehaviour
         AudioListener.volume = 0f;
 
         m_Paused = true;
+        pauseMenu.SetActive(true);
+        gameUI.SetActive(false);
+        
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
 
@@ -33,29 +41,44 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = m_TimeScaleRef;
         AudioListener.volume = m_VolumeRef;
         m_Paused = false;
+        pauseMenu.SetActive(false);
+        gameUI.SetActive(true);
+        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
     public void OnMenuStatusChange ()
     {
-        if (m_MenuToggle.isOn && !m_Paused)
+        if (!m_Paused) //m_MenuToggle.isOn && 
         {
             MenuOn();
         }
-        else if (!m_MenuToggle.isOn && m_Paused)
+        else if (m_Paused) //!m_MenuToggle.isOn && 
         {
             MenuOff();
         }
     }
 
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 
 #if !MOBILE_INPUT
 	void Update()
 	{
-		if(Input.GetKeyUp(KeyCode.Escape))
+		if(Input.GetKeyDown(KeyCode.Escape))
 		{
-		    m_MenuToggle.isOn = !m_MenuToggle.isOn;
-            Cursor.visible = m_MenuToggle.isOn;//force the cursor visible if anythign had hidden it
+            OnMenuStatusChange();
+		    /*m_MenuToggle.isOn = !m_MenuToggle.isOn;
+            Cursor.visible = m_MenuToggle.isOn;//force the cursor visible if anythign had hidden it*/
 		}
 	}
 #endif
