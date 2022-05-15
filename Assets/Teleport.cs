4e68830dab3324ced71aceb2e25bac5292897653
Erick Vaghi <91052540+ErickVaghi@users.noreply.github.com>
2022-05-15 21:07:07 +0200

    car teleport fixed

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -11,13 +11,14 @@ public class Teleport : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.P))
         {
              playerPosition.position = playerCheckPoint.position;
         }
-        if (Input.GetKeyDown(KeyCode.G))
+        if (Input.GetKeyDown(KeyCode.O))
         {
             carPosition.position = carCheckPoint.position;
+            carPosition.rotation = new Quaternion(0, 0, 0, 0);
         }
     }
 }
