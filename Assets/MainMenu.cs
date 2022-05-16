b8c347fd7c92de5ec7490866e33937338281e6c7
Arvid Lundin <arvid.lundin@student.forsbergsskola.se>
2022-05-16 19:49:11 +0200

    main menu audio

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public FMODUnity.EventReference mainMenuAudioRef;
+    private FMOD.Studio.EventInstance mainMenuAudioInst;
+
+    public void Start()
+    {
+        mainMenuAudioInst = FMODUnity.RuntimeManager.CreateInstance(mainMenuAudioRef);
+        mainMenuAudioInst.start();
+    }
+
+
+
     public void NewGame(int i)
     {
         SceneController.LoadScene(i, 1, 2);
+        mainMenuAudioInst.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     }
 
     public void Quit()
